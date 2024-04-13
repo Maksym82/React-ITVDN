4c99e1efc41132c982360d314cff1499ddf08fde
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-04-13 20:28:17 +0300

    update styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import React from 'react';
+import './App.css';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+
+  const el1 = React.createElement('h1', {
+    'id': 123,
+    'style': {
+      'fontSize' : 45,
+      'color': 'green',
+      'background' : 'brown'
+    }
+  }, 'some text');
+
+  const parag = React.createElement('p', {}, 'textcmfkvnksdnvksd');
+  const block = React.createElement('div', {}, parag);
+    return (
+    <>
+        {el1}
+        <h1 id={123} style={{ fontSize: 45, color: 'green', background: 'brown' }}>some text</h1>
+        {block}
+    </>
   );
 };
