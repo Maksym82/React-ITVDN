d12243d03d766cd8c43298d363778f61f7530045
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-04-14 12:18:38 +0300

    Update app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import './App.css';
+import logo from '../image/logo192.png';
 
 export const App = () => {
 
-  const el1 = React.createElement('h1', {
-    'id': 123,
-    'style': {
-      'fontSize' : 45,
-      'color': 'green',
-      'background' : 'brown'
-    }
-  }, 'some text');
-
-  const parag = React.createElement('p', {}, 'textcmfkvnksdnvksd');
+  const parag = React.createElement('p', {}, 'text about home work');
   const block = React.createElement('div', {}, parag);
+ 
     return (
     <>
-        {el1}
-        <h1 id={123} style={{ fontSize: 45, color: 'green', background: 'brown' }}>some text</h1>
-        {block}
+      
+      <h1 id={123} style={{ fontSize: 45, color: 'green', background: 'brown' }}>Title</h1>
+      {block}
+      <img src={logo} className='logo' alt="" />
+              
     </>
   );
 };
+
+
